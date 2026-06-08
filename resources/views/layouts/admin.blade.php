5ac9a4de8b4070e9a5e6aacf94b16d7e16ad612f
Ashik <corporatecare33@gmail.com>
2026-06-08 19:26:20 +0600

    Fix admin logout - replace Livewire component with direct form

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -119,9 +119,14 @@
                                     </div>
                                 </button>
                             </div>
-                            <div x-show="open" @click.away="open = false" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                            <div x-show="open" @click.away="open = false" x-transition class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" style="display: none;">
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
-                                <livewire:admin.logout-button />
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                                        Sign out
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
